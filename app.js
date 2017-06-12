4e56c937a42239aacd4cb292329bbb54ee3f1585
Jason L <jluboff@gmail.com>
2017-06-12 16:51:31 -0700

    Changed from local MongoDb to mLab and changed to req.query

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ const port = process.env.PORT || 3000;
 
 var app = express();
 
-MongoClient.connect('mongodb://localhost:27017/quotes', (err, db) => {
-    if(err) throw err;
+MongoClient.connect(`mongodb://test:test@ds123752.mlab.com:23752/simpsonsquotes`, (err, db) => {
+  if(err) throw err;
 
-    app.get('/:num', (req, res) => {
-      let num = Number(req.params.num);
+  app.get('/quotes', (req, res) => {
+    let num = Number(req.query.count);
 
-      num = !num ? 1 : num > 10 ? 10: num;
-      db.collection('quote').aggregate([{ $sample : {size : num }}], (err, doc) => {
-        res.setHeader('Content-Type', 'application/json');
-     res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    num = !num ? 1 : num > 10 ? 10: num;
+    db.collection('quotes').aggregate([{ $sample : {size : num }}], (err, doc) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-        res.send(doc);
-        console.log(doc);
-      })
-    });
+      res.send(doc);
+
+    })
+  });
 
 
-
+  
   app.listen(port, () =>{
     console.log(`Listening on port ${port}`);
   });
