bb4396ed22ad7e072435b64374e8f13d0b5988f8
Jason L <jluboff@gmail.com>
2022-02-04 09:36:55 -0800

    Refactored to include character query search within aggregate. Removed callbacks in favor of async/await. Removed 10 quote max limit. Added comments.

--- app.js
+++ app.js
@@ -2,51 +2,74 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const port = process.env.PORT || 3000;
 
-var app = express();
+const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-MongoClient.connect(
-  `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@jl.qmwzj.mongodb.net/simpsonsquotes`,
-  (err, client) => {
-    if (err) throw err;
+app.get('/quotes', async (req, res) => {
+  try {
+    /**
+     * Connect to the database
+     */
+    const client = await MongoClient.connect(
+      `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@jl.qmwzj.mongodb.net/simpsonsquotes`
+    );
+    const db = client.db('simpsonsquotes');
+    /**
+     * Get the count of quotes requested and the character from query params
+     */
+    const {
+      query: { count, character },
+    } = req;
+    const numOfQuotes = Number(count) || 1;
 
-    app.get('/quotes', (req, res) => {
-      const db = client.db('simpsonsquotes');
-      let num = Number(req.query.count) || 1;
+    /**
+     * Create aggregate to limit the number of quotes returned by provided num
+     * If no num is provided, return 1 quote
+     */
+    const aggregate = [
+      { $sample: { size: numOfQuotes } },
+      {
+        $project: {
+          _id: 0,
+          character: 1,
+          quote: 1,
+          image: 1,
+          characterDirection: 1,
+        },
+      },
+    ];
+    /**
+     * If a character is provided, add a regex match (allows for partial names) to the aggregate
+     */
+    if (character) {
+      aggregate.splice(0, 0, {
+        $match: { character: { $regex: new RegExp(character, 'i') } },
+      });
+    }
+    /**
+     * Get the quotes from the database
+     */
+    const documents = await db.collection('quotes').aggregate(aggregate).toArray();
+    /**
+     * Send the quotes to the client
+     */
+    res.setHeader('Content-Type', 'application/json');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
 
-      num = num > 10 ? 10 : num;
-      db.collection('quotes').aggregate(
-        [
-          { $sample: { size: num } },
-          {
-            $project: {
-              _id: 0,
-              character: 1,
-              quote: 1,
-              image: 1,
-              characterDirection: 1,
-            },
-          },
-        ],
-        async (error, docCursor) => {
-          if (error) throw error;
-          const documents = [];
-          await docCursor.forEach((document) => documents.push(document));
-          res.setHeader('Content-Type', 'application/json');
-          res.header('Access-Control-Allow-Origin', '*');
-          res.header(
-            'Access-Control-Allow-Headers',
-            'Origin, X-Requested-With, Content-Type, Accept'
-          );
-
-          res.send(documents);
-        }
-      );
-    });
-
-    app.listen(port, () => {
-      console.log(`Listening on port ${port}`);
-    });
+    res.send(documents);
+  } catch (error) {
+    /**
+     * If an error occurs, send the error to the client
+     */
+    res.json(error.message);
   }
-);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
