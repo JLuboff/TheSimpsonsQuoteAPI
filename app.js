c1ec78a02d17ecfa22b5e4837ffeb9ae103402a5
samjsolomon <samjsolomon@aol.com>
2024-02-03 12:19:21 -0500

    remove mongo in favor of local json

--- app.js
+++ app.js
@@ -1,75 +1,43 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+const fs = require('fs');
+const path = require('path');
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/quotes', async (req, res) => {
+const quotesFilePath = path.join(__dirname, 'quotes.json');
+const quotes = JSON.parse(fs.readFileSync(quotesFilePath, 'utf8'));
+
+// Fisher-Yates (Knuth) shuffle algorithm
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+app.get('/quotes', (req, res) => {
   try {
-    /**
-     * Connect to the database
-     */
-    const client = await MongoClient.connect(
-      `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@jl.qmwzj.mongodb.net/simpsonsquotes`
-    );
-    const db = client.db('simpsonsquotes');
-    /**
-     * Get the count of quotes requested and the character from query params
-     */
     const {
       query: { count, character },
     } = req;
     const numOfQuotes = Number(count) || 1;
 
-    /**
-     * Create aggregate to limit the number of quotes returned by provided num
-     * If no num is provided, return 1 quote
-     */
-    const aggregate = [
-      { $sample: { size: numOfQuotes } },
-      {
-        $project: {
-          _id: 0,
-          character: 1,
-          quote: 1,
-          image: 1,
-          characterDirection: 1,
-        },
-      },
-    ];
-    /**
-     * If a character is provided, add a regex match (allows for partial names) to the aggregate
-     */
-    if (character) {
-      aggregate.splice(0, 0, {
-        $match: { character: { $regex: new RegExp(character, 'i') } },
-      });
-    }
-    /**
-     * Get the quotes from the database
-     */
-    const documents = await db.collection('quotes').aggregate(aggregate).toArray();
-    /**
-     * Close the connection to the database
-     */
-    client.close();
-    /**
-     * Send the quotes to the client
-     */
+    // Shuffle the array of quotes
+    const shuffledQuotes = shuffleArray(quotes);
+
+    // Select a subset of quotes
+    const selectedQuotes = shuffledQuotes.slice(0, numOfQuotes);
+
     res.setHeader('Content-Type', 'application/json');
     res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
-    res.send(documents);
+    res.send(selectedQuotes);
   } catch (error) {
-    /**
-     * If an error occurs, send the error to the client
-     */
     res.json(error.message);
   }
 });
